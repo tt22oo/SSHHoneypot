e14020b94721bb374e8af07fe30eabdb6f2fc949
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-02-08 11:31:42 +0900

    Enhance Ls function to support '-a' argument

--- core/commands/file/ls.go
+++ core/commands/file/ls.go
@@ -9,10 +9,22 @@ import (
 func Ls(s *session.Session, args []string) (string, int) {
 	var output string
 	for name := range s.Entry.Children {
-		if strings.HasPrefix(name, ".") {
-			continue
+		if len(args) >= 2 {
+			switch args[1] {
+			case "-a":
+				output += fmt.Sprintf("%s ", name)
+			default:
+				if strings.HasPrefix(name, ".") {
+					continue
+				}
+				output += fmt.Sprintf("%s ", name)
+			}
+		} else {
+			if strings.HasPrefix(name, ".") {
+				continue
+			}
+			output += fmt.Sprintf("%s ", name)
 		}
-		output += fmt.Sprintf("%s ", name)
 	}
 
 	if output != "" {
