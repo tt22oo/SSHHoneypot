33854a1217182a1efef2bea427644476c939d5e0
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-02-15 16:39:19 +0900

    Add logging for errors and server status

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"honeypot/core/commands"
 	"honeypot/core/configs"
 	"honeypot/core/session/handler"
+	"log"
 
 	"github.com/gliderlabs/ssh"
 )
 
 func main() {
+	log.Println("[-----------------------------------------]")
 	err := configs.Read()
 	if err != nil {
-		panic(err)
+		log.Printf("\033[31m[ERROR]\033[0m Read Config Error: %s\r\n", err.Error())
+		return
 	}
 
 	commands.Init()
@@ -25,7 +28,13 @@ func main() {
 
 	if configs.Cfg.Config.Auth.Auth {
 		server.PasswordHandler = auth.Auth
+		log.Println(" \033[34m[INFO]\033[0m Password Authentication Enabled")
 	}
 
-	panic(server.ListenAndServe())
+	log.Printf(" \033[34m[INFO]\033[0m Server Listening on Port %s\r\n", configs.Cfg.Config.Listen)
+	log.Println("[-----------------------------------------]")
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Printf("\033[31m[ERROR]\033[0m Server Error: %s", err.Error())
+	}
 }
