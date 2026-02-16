6932216b4d7f1166c5b87f5253c87123de9b262b
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-02-16 10:14:28 +0900

    Remove visual separator log statements
    Removed log statements for visual separators in the output.

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 )
 
 func main() {
-	log.Println("[-----------------------------------------]")
 	err := configs.Read()
 	if err != nil {
 		log.Printf("\033[31m[ERROR]\033[0m Read Config Error: %s\r\n", err.Error())
@@ -32,7 +31,6 @@ func main() {
 	}
 
 	log.Printf(" \033[34m[INFO]\033[0m Server Listening on Port %s\r\n", configs.Cfg.Config.Listen)
-	log.Println("[-----------------------------------------]")
 	err = server.ListenAndServe()
 	if err != nil {
 		log.Printf("\033[31m[ERROR]\033[0m Server Error: %s", err.Error())
