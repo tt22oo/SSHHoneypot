b7981bcc6e0c4c868b21499bf9c8ab673864b03e
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-02-23 15:26:20 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-	"honeypot/core/auth"
-	"honeypot/core/commands"
-	"honeypot/core/configs"
-	"honeypot/core/session/handler"
+	"honeypot/core/server"
 	"log"
-
-	"github.com/gliderlabs/ssh"
 )
 
 func main() {
-	err := configs.Read()
+	err := server.StartSSH()
 	if err != nil {
-		log.Printf("\033[31m[ERROR]\033[0m Read Config Error: %s\r\n", err.Error())
+		log.Printf(" \033[31m[ERROR]\033[0m %s\r\n", err.Error())
 		return
-	}
-
-	commands.Init()
-
-	server := &ssh.Server{
-		Addr:    configs.Cfg.Config.Listen,
-		Handler: handler.Handler,
-		Version: configs.Cfg.Config.Banner,
-	}
-
-	if configs.Cfg.Config.Auth.Auth {
-		server.PasswordHandler = auth.Auth
-		log.Println(" \033[34m[INFO]\033[0m Password Authentication Enabled")
-	}
-
-	log.Printf(" \033[34m[INFO]\033[0m Server Listening on Port %s\r\n", configs.Cfg.Config.Listen)
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Printf("\033[31m[ERROR]\033[0m Server Error: %s", err.Error())
 	}
 }
