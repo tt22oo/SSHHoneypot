2f36deb92497673eb518a27ec587e5207e479bc6
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-02-23 10:14:49 -0800

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"honeypot/core/server"
-	"log"
 )
 
 func main() {
-	err := server.StartSSH()
-	if err != nil {
-		log.Printf(" \033[31m[ERROR]\033[0m %s\r\n", err.Error())
-		return
-	}
+	go server.StartSSH() // start ssh honeypot
+	select {}
 }
